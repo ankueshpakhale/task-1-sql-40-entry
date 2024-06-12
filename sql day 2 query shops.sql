a4b2ac788aa49baa10ded577e6001f4855248739
ankueshpakhale <ankushpakhale786@gmail.com>
2024-06-12 17:37:07 +0530

    Add files via upload

---
+++ sql day 2 query shops.sql
@@ -0,0 +1,59 @@
+create table shops(
+	id int primary key,
+	name varchar(150),
+	no_of_items int,
+	category varchar(15),
+	no_of_emp int,
+	owner_name varchar(150),
+	address varchar (150),
+	mobile_no int,
+	sales int,
+	revenue int,
+	is_active boolean
+) 
+
+select * from shops
+
+insert into shops values  (1,' geeta sons',327,'retail',65,'geeta pakhale','ramsumerbaba nagar',6556,550000,100000,true);
+insert into shops values (2,'nishad',78,'retail',65,'nishad','kamal chowk',8787,178000,67900,true);
+insert into shops values (3,'jitesh arts',86,'retail',10,'jitesh mohan','abhyankar nagar',7575,135000,50000,true);
+insert into shops values (4,'chandracollection',75,'retail',25,'simran chandra','sai nagar',8878,150000,40000,true);
+insert into shops values (5,'jeet medical',150,'retail',35,'hitesh','narendra nagar',9898,250000,750000,true);
+insert into shops values (6,'gayatri articals',100,'wholesale',25,'dikshita','kamal chowk',9090,500000,125000,true);
+insert into shops values (7,'kumar sons',75,'retail',12,'raman kumar','sai nagar',8787,700000,135000,true);
+insert into shops values (8,'tripathi paints',135,'retail',23,'karuna tripathi','kampthi',4534,159000,60000,true);
+insert into shops values (9,'akasa textiles',76,'wholesale',45,'anita mukharji','abhyankar nagar',5556,500000,120000,true);
+insert into shops values (10,'yesankar machines',15,'local',5,'pralay yesankar','mohan nagar',8989,120000,45000,true);
+insert into shops values (11,'rose parlour',50,'local',10,'yamini','ekta nagar',8765,500000,175000,true);
+insert into shops values (12,'jeet medical',150,'retail',25,'sameer mohan','vishvakarma nagar',9998,600000,120000,true);
+insert into shops values (13,'pakhale plastic',200,'wholesale',50,'ankush pakhale','mohan nagar',0786,700000,150000,true);
+insert into shops values (14,'karishma garments',70,'local',12,'karishma thakur','kamal chowk',8888,150000,35000,true);
+insert into shops values (15,'goel cooler',45,'retail',10,'suresh goel','gandhibaug',7654,650000,175000,true);
+insert into shops values (16,'pandya store',75,'local',10,'sachin kamat','kamal chowk',3214,150000,40000,true);
+insert into shops values (17,'wasim store',60,'local',9,'wasim khan','samta nagar',7676,500000,100000,true);
+insert into shops values (18,'anita garments',15,'local',4,'anita parate','mohan nagar',9090,50000,10000,true);
+insert into shops values (19,'diksha cakes',10,'local',2,'diksha thul','gandhi nagar',7686,50000,20000,true);
+insert into shops values (20,'aswadeep textiles',55,'wholesale',12,'gita fugad','narendra nagar',9090,550000,123000,true);
+insert into shops values (21,'gajanan hardware',76,'local',2,'ramesh hedau','sai chowk',7654,650000,30000,true);
+insert into shops values (22,'agrawal sons',80,'retail',20,'niti agrawal','rankrishna nagar',8900,780000,200000,true);
+insert into shops values (23,'krushna electricals',150,'wholesale',40,'roshan tripathi','ganga nagar',1234,1000000,300000,true);
+insert into shops values (24,'roopa parlour',65,'local',15,'roopa likhar','mohan nagar',8989,175000,35000,true);
+insert into shops values(25,'bansi dairy',40,'local',10,'rajkumar','tandapeth',7777,150000,40000,true);
+insert into shops values (26,'shubham stores',90,'retail',12,'shubham','shanti nagar',9098,500000,10000,true);
+insert into shops values (27,'krish cakes',8,'local',2,'greshi pakhale','golibar chowk',0712,70000,30000,true);
+insert into shops values(29,'himani garments',60,'local',7,'himani raut','raut chowk',9642,80000,45000,true);
+insert into shops values (30,'nikhil solutions',50,'wholesale',25,'krishna kamat','yewale apartments',6567,1300000,400000,true);
+insert into shops values(31,'vidisha textiles',46,'wholesale',30,'garima','lalwani nagar',9712,700000,125000,true);
+insert into shops values (32,'gokul dairy',50,'retail',10,'yash gupta','jagnade chowk',8874,300000,75000,true);
+insert into shops values (33,'chanduram bekary',150,'retail',25,'yagnesh shah','telephone exchange',5543,800000,250000,true);
+insert into shops values (34,'shivshakti bar',45,'local',15,'shankar pakhale','gokul dham',7654,500000,125000,true);
+insert into shops values (35,'girja sweets',80,'retail',15,'swati maheshwari','vaishali nagar',8787,450000,123000,true);
+insert into shops values(36,'radha dairy',75,'local',30,'durgawati nagar','moohan nagar',9098,500000,10000,true);
+insert into shops values (37,'lalu sweets',90,'local',20,'ram gupta','gaddigodam',7868,575000,125000,true);
+insert into shops values(38,'chotelal halwai',75,'local',5,'ashwin mehta','vaishali nagar',6767,800000,250000,true);
+insert into shops values(39,'mehata sweets',120,'retail',10,'shivansh mehta','vaishali nagar',1212,750000,250000,true);
+insert into shops values(40,'street9cafe',35,'local',5,'vivek limje','laxminagar',8976,500000,150000,true)
+
+
+select * from shops
+		s
